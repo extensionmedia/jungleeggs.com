3b84982d625fb06b39c0f56178f048909cc25e66
yassine_desktop <elmeftouhi@extensionmedia.ma>
2022-04-15 00:17:22 +0100

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>{{config('app.name')}}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="w-screen h-screen" style="background-color: #f8f8f8; font-family: 'Lato', sans-serif;">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+@section('content')
+    <div class="w-full h-full bg-black bg-center bg-cover" style="background-repeat:no-repeat; background-image: url({{asset('images/bg.jpg')}});">
+        ddd
+    </div>
+@endsection
